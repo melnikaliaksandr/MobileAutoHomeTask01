6b61258d04c6ab2ad54ff3435823fa91000511d0
Aliaksandr Melnik <melnik.alexander.mail@gmail.com>
2020-02-24 23:48:39 +0300

    Exercise #2

--- src/test/MainClassTest.java
+++ src/test/MainClassTest.java
@@ -12,4 +12,10 @@ public class MainClassTest {
         int expected = 14;
         Assert.assertEquals("Wrong number!", expected, mainClass.getLocalNumber());
     }
+
+    @Test
+    public void testGetClassNumber() {
+        MainClass mainClass = new MainClass();
+        Assert.assertTrue("Number less than 45", mainClass.getClassNumber() > 45);
+    }
 }
