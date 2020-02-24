abcf385655b46377f40884591d09a8874c0aa02d
Aliaksandr Melnik <melnik.alexander.mail@gmail.com>
2020-02-25 00:03:16 +0300

    Exercise #3

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -2,7 +2,12 @@ package main;
 
 public class MainClass {
 
+    private String class_string = "Hello, world";
     private int class_number = 20;
+
+    public String getClassString() {
+        return class_string;
+    }
 
     public int getClassNumber() {
         return class_number;
--- src/test/MainClassTest.java
+++ src/test/MainClassTest.java
@@ -18,4 +18,12 @@ public class MainClassTest {
         MainClass mainClass = new MainClass();
         Assert.assertTrue("Number less than 45", mainClass.getClassNumber() > 45);
     }
+
+    @Test
+    public void testGetClassString() {
+        MainClass mainClass = new MainClass();
+        Assert.assertTrue("The string does not contains \"hello\" or \"Hello\"",
+                mainClass.getClassString().contains("hello")
+                        || mainClass.getClassString().contains("Hello"));
+    }
 }
